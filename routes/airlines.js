72420b040039eed9c3f9f1649a95330cb513bde3
Bishop Shaw III <prototype124@yahoo.com>
2024-06-19 19:57:26 -0700

    updated files

--- routes/airlines.js
+++ routes/airlines.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const Router = express.Router({ mergeParams: true });
 const routes = require("../controllers/airlines");
+const serverless = require("serverless-http");
 
-Router.get("/", routes.index);
+// Router.get("/", routes.index);
 Router.get("/info", routes.destinationIndex);
 Router.get("/:slug", routes.showPage);
 
-module.exports = Router;
+module.exports.handler = serverless(Router);
+// module.exports = Router;
