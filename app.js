bb08b1c0de9f4494031567ce00aa9faefcf111ae
Bishop Shaw III <prototype124@yahoo.com>
2024-07-03 18:39:53 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const server = require("serverless-http");
+const cors = require("cors");
 const airlineRoutes = require("./routes/airlines");
 const destinationRoutes = require("./routes/destinations");
 require("./databaseConnection");
-// const PORT = process.env.port || 8080;
+const PORT = process.env.port || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,4 +15,4 @@ app.use("/airlines", airlineRoutes);
 app.use("/airlines/info", airlineRoutes);
 app.use("/airlines/:slug/destinations", destinationRoutes);
 
-// app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
