78a4bb8be684ab4136af8b79ef65be04c86c7404
Bishop Shaw III <prototype124@yahoo.com>
2024-07-03 18:42:02 -0700

    deleted handler.js file

--- handler.js
+++
@@ -1,5 +0,0 @@
-const serverless = require("serverless-http");
-const app = require("./app"); // main Express app file is named app.js
-
-// Export the serverless handler function
-module.exports.handler = serverless(app);
